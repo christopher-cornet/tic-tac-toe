5b2f17cf81cfbf59c345270efbd332ce619c822c
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-01-13 10:33:05 +0100

    Scoreboard et users

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,4 @@
-print("Tic Tac Toe en Python !")
+print("Tic Tac Toe en Python !\n")
 
 possibleNumbers = [1,2,3,4,5,6,7,8,9]
 gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
@@ -7,6 +7,9 @@ columns = 3
 
 leaveLoop = False
 turnCounter = 0
+
+j1 = input("Pseudo Joueur 1: ")
+j2 = input("Pseudo Joueur 2: ")
 
 # Fonction pour afficher le tableau de jeu
 def printGameBoard():
@@ -48,193 +51,164 @@ def checkForWinner(gameBoard):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][0] == "O" and gameBoard[0][1] == "O" and gameBoard[0][2] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     elif(gameBoard[1][0] == "X" and gameBoard[1][1] == "X" and gameBoard[1][2] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[1][0] == "O" and gameBoard[1][1] == "O" and gameBoard[1][2] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     elif(gameBoard[2][0] == "X" and gameBoard[2][1] == "X" and gameBoard[2][2] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[2][0] == "O" and gameBoard[2][1] == "O" and gameBoard[2][2] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     # Axe Y
     if(gameBoard[0][0] == "X" and gameBoard[1][0] == "X" and gameBoard[2][0] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][0] == "O" and gameBoard[1][0] == "O" and gameBoard[2][0] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     elif(gameBoard[0][1] == "X" and gameBoard[1][1] == "X" and gameBoard[2][1] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][1] == "O" and gameBoard[1][1] == "O" and gameBoard[2][1] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     elif(gameBoard[0][2] == "X" and gameBoard[1][2] == "X" and gameBoard[2][2] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][2] == "O" and gameBoard[1][2] == "O" and gameBoard[2][2] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     # Victoire en diagonale
     if(gameBoard[0][0] == "X" and gameBoard[1][1] == "X" and gameBoard[2][2] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][0] == "O" and gameBoard[1][1] == "O" and gameBoard[2][2] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O"
     elif(gameBoard[0][2] == "X" and gameBoard[1][1] == "X" and gameBoard[2][0] == "X"):
         scorej1 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 1 (X) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j1 + " " + "(X) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "X"
     elif(gameBoard[0][2] == "O" and gameBoard[1][1] == "O" and gameBoard[2][0] == "O"):
         scorej2 += 1
         scorej1str = str(scorej1)
         scorej2str = str(scorej2)
-        print("Joueur 2 (O) a gagné!")
-        print("ScoreBoard:")
-        print("Joueur 1: " + scorej1str)
-        print("Joueur 2: " + scorej2str)
+        print(j2 + " " + "(O) a gagné!")
+        print("\nScoreBoard:")
+        print(j1 + " " + scorej1str)
+        print(j2 + " " + scorej2str)
         return "O" 
     else:
         return "Match nul"
 
 def startGame():
-    leaveLoop = False
-    while(leaveLoop == False):
-        possibleNumbers = [1,2,3,4,5,6,7,8,9]
-        gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
-        gameBoardvoid = [[1,2,3], [4,5,6], [7,8,9]]
-        rows = 3
-        columns = 3
-        turnCounter = 0
-
-        # Tour du joueur
-        if(turnCounter % 2 == 0):
-            printGameBoard()
-            numberPicked = int(input("\n(Joueur 1 | X) Chosis un nombre [1-9]: "))
-            if(numberPicked >= 1 or numberPicked <= 9):
-                modifyList(numberPicked, 'X')
-                possibleNumbers.remove(numberPicked)
-            else:
-                print("Nombre invalide, essaie encore.")
-            turnCounter += 1
-            printGameBoard()
-        # Tour de l'ordinateur
-        else:
-            while(True):
-                numberPicked = int(input("\n(Joueur 2 | O) Chosis un nombre [1-9]: "))
-                if(numberPicked in possibleNumbers):
-                    modifyList(numberPicked, 'O')
-                    possibleNumbers.remove(numberPicked)
-                    turnCounter += 1
-                    break
-
-        winner = checkForWinner(gameBoard)
-        if(winner != "Match nul"):
-            print("\nPartie terminée.")
-            print("\nVoulez vous rejouer ?")
-            break
+    possibleNumbers = [1,2,3,4,5,6,7,8,9]
+gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
+gameBoardvoid = [[1,2,3], [4,5,6], [7,8,9]]
+rows = 3
+columns = 3
+turnCounter = 0
 
 while(leaveLoop == False):
   # Tour du joueur 1
   if(turnCounter % 2 == 0):
     printGameBoard()
-    numberPicked = int(input("\n(Joueur 1 | X) Chosis un nombre [1-9]: "))
+    numberPicked = int(input("\n" + "(" + j1 + " | X)" + " Chosis un nombre [1-9]: "))
     if(numberPicked >= 1 or numberPicked <= 9):
       modifyList(numberPicked, 'X')
       possibleNumbers.remove(numberPicked)
@@ -245,7 +219,7 @@ while(leaveLoop == False):
   # Tour du joueur 2
   else:
     while(True):
-      numberPicked = int(input("\n(Joueur 2 | O) Chosis un nombre [1-9]: "))
+      numberPicked = int(input("\n" + "(" + j2 + " | O)" + " Chosis un nombre [1-9]: "))
       if(numberPicked in possibleNumbers):
         modifyList(numberPicked, 'O')
         possibleNumbers.remove(numberPicked)
@@ -255,11 +229,12 @@ while(leaveLoop == False):
   winner = checkForWinner(gameBoard)
   if(winner != "Match nul"):
     print("\nPartie terminée.")
-    print("\nVoulez vous rejouer ? yes/oui ou no/non")
+    print("Voulez vous rejouer ? oui/non\n")
     remake = input("")
-    if remake == "yes" or "oui":
-        startGame()
+    if remake == "oui":
+        print("\nCa marche pas ^^' ")
+        exit()
     else:
-        quit
+        exit()
 
 startGame()
